9be24617389f35341ef16bd17c5fbade6fc91179
Gabriel Barros <gabriel.fbarros@yahoo.com.br>
2017-07-21 17:37:24 -0300

    Inclui dados do endereco.

--- src/DotNetCoreAppExample.Infra.CrossCutting.Identity/Data/ApplicationDbContext.cs
+++ src/DotNetCoreAppExample.Infra.CrossCutting.Identity/Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using DotNetCoreAppExample.Infra.CrossCutting.Identity.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace DotNetCoreAppExample.Infra.CrossCutting.Identity.Data
 {
@@ -11,12 +13,14 @@ namespace DotNetCoreAppExample.Infra.CrossCutting.Identity.Data
         {
         }
 
-        protected override void OnModelCreating(ModelBuilder builder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
         }
     }
 }
