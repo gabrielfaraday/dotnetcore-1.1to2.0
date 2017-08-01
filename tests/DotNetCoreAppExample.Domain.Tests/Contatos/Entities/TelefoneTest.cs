b3a8c79cd0334b7d625e8bac4509d21d3ff2d5fb
Gabriel Barros <gabriel.fbarros@yahoo.com.br>
2017-08-01 18:34:14 -0300

    Inicia testes de unidade.

---
+++ tests/DotNetCoreAppExample.Domain.Tests/Contatos/Entities/TelefoneTest.cs
@@ -0,0 +1,22 @@
+﻿using DotNetCoreAppExample.Domain.Contatos.Entities;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace DotNetCoreAppExample.Domain.Tests.Contatos.Entities
+{
+    [TestFixture]
+    public class TelefoneTest
+    {
+        [Test]
+        public void Telefone_DDD_DeveTerDoisCaracteres()
+        {
+            var telefone = new Telefone(Guid.NewGuid(), "333", "33334455", Guid.NewGuid());
+
+            telefone.EstaValido();
+
+            Assert.IsFalse(telefone.ValidationResult.IsValid);
+            Assert.IsTrue(telefone.ValidationResult.Errors.Any(e => e.ErrorMessage == "DDD deve ter 2 caracteres"));
+        }
+    }
+}
